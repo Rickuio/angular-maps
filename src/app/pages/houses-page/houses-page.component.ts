457fbf2925618d9b06cd8be01c412762e4ef5d24
angel_anchaluisa <angel_anchaluisa@trabajo.gob.ec>
2025-06-23 11:42:35 -0500

    Iniciando aplicacion de mapas en Angular

---
+++ src/app/pages/houses-page/houses-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-houses-page',
+  imports: [],
+  templateUrl: './houses-page.component.html',
+  styleUrl: './houses-page.component.css',
+})
+export class HousesPageComponent { }
